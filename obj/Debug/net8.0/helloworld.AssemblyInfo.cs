8accdaed47faef80b73ef8fd136abb34e3e7ceec
Darshan <darshikr98@gmail.com>
2024-07-15 10:50:50 -0700

    Commit from Windows for C# Practice

--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a93151f06a5e0cbddeebc0b94364cdd78c5345")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d1663e05e070858a21dfd3e2f98422c2889e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
