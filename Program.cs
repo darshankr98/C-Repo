c3af6a5fcdacd92d67a5daf71b8a118af450844d
Darshan <darshikr98@gmail.com>
2024-07-15 23:47:03 +0530

    recent

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string firstFriend = "Maria";
-string secondFriend = "Sage";
-//int a = 214748364;
-int result = 3 + 1 * 5 / 2;
-Console.WriteLine(result);
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// string firstFriend = "Maria";
+// string secondFriend = "Sage";
+// //int a = 214748364;
+// int result = 3 + 1 * 5 / 2;
+// Console.WriteLine(result);
+// Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
 
 
-/*for (int i = 0;i<5;i++){
-    for(char j = 'a';j<'k';j++){
-        Console.Write($"The string is {i},{j}");
-    }
-}*/
+// /*for (int i = 0;i<5;i++){
+//     for(char j = 'a';j<'k';j++){
+//         Console.Write($"The string is {i},{j}");
+//     }
+// }*/
